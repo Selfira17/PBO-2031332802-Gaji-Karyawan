48f58bcc169312072c83df3fa9bfa3904ba972fe
selfira <selfiraayw@gmail.com>
2021-03-31 22:33:29 +0700

    PBO-Latihan51 Gaji Karyawan

---
+++ src/pbo/pkg2031332802/gaji/karyawan/PBO2031332802GajiKaryawan.java
@@ -0,0 +1,74 @@
+/*
+Nama    : Selfira Anyuniarwati
+Kelas   : PBO (FS112B)
+NIM     : 2031332802
+Deskripsi : Latihan 51-Gaji karyawan 
+ */
+package pbo.pkg2031332802.gaji.karyawan;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PBO2031332802GajiKaryawan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+     int getNik = 110110290;
+   String getName = "Rizki Adam kurniawan";
+   int getGolongan = 2;
+   String getJabatan = "Manager";
+   int getKehadiran = 30;
+   
+   
+   System.out.println("Masukan Nama   :"+getName);
+   System.out.println("Masukan Nik   :"+getNik);
+   System.out.println("Masukan Golongan 1/2/3   :"+getGolongan);
+   System.out.println("Masukan Jabatan (Manager/Kabag)   :"+getJabatan);
+   System.out.println("Masukan Jumlah Kehadiran  :"+getKehadiran);
+   
+   
+   
+   System.out.println("========================================================");
+
+   float TunjanganGolongan = 1000000;
+   float TunjanganJabatan = 2000000;
+   float TunjanganKehadiran = 3000000;
+   
+   float GajiTotal = 3300000;
+   
+   System.out.println("Tunjangan Golongan  :"+TunjanganGolongan);
+   System.out.println("Tunjangan Jabatan  :"+TunjanganJabatan);
+   System.out.println("Tunjangan Golongan  :"+TunjanganGolongan);
+   System.out.println("Tunjangan Kehadiran  :"+TunjanganKehadiran);
+   
+   System.out.println("Gaji Total  :"+GajiTotal);
+
+
+        
+        
+        
+      
+    
+       
+       
+       
+          
+        
+        
+               
+                
+                
+                
+        
+      
+        
+
+                
+        
+    }
+    
+}
